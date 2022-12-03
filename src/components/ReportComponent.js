166438a92e1f4ac740fbd30964ee3239c8649590
Kritsanachai Ngoennak (ITD # 1602) <Kritsanachai@tpppack.com>
2022-12-03 12:58:29 +0700

    V1.49_TPP 2.52

--- src/components/ReportComponent.js
+++ src/components/ReportComponent.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import DataContext from "./data/DataContext";
+import './ReportComponent.css'
 
 const ReportComponent = () => {
   const { income, expense } = useContext(DataContext);
@@ -7,6 +8,19 @@ const ReportComponent = () => {
     <div>
      <h3>ยอดคงเหลือ (บาท)</h3>
      <h1>{income-expense}</h1>
+      <div className="report-container">
+        <div>
+          <h4>ยอดรายรับ</h4>
+          <p className="report-income">{income}</p>
+        </div>
+        <div>
+        <h4>ยอดรายจ่าย</h4>
+        <p className="report-expense">{expense}</p>
+        </div>
+
+
+      </div>
+   
     </div>  
   );
 };
